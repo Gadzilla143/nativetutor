8b52ca4d2d4be3e5649aeb8efd106b5d00601c35
Darya.Liashchynskaya <Dashalesh040600>
2022-04-14 18:25:27 +0300

    pages data consts

---
+++ src/constants/page_constants.js
@@ -0,0 +1,39 @@
+export const travel = require('../assets/pagesIcons/travel.png');
+export const discount = require('../assets/pagesIcons/discount.png');
+export const fruits = require('../assets/pagesIcons/fruits.png');
+export const health = require('../assets/pagesIcons/healthcare.png');
+export const humanResources = require('../assets/pagesIcons/humanResources.png');
+export const technicalGuide = require('../assets/pagesIcons/technicalGuide.png');
+
+export const PAGES_DATA = [
+  {
+    id: 1,
+    icon: discount,
+    pageName: 'Discount Club',
+  },
+  {
+    id: 2,
+    icon: travel,
+    pageName: 'Travel',
+  },
+  {
+    id: 3,
+    icon: fruits,
+    pageName: 'Fruits Delivery',
+  },
+  {
+    id: 4,
+    icon: health,
+    pageName: 'Safety and Health',
+  },
+  {
+    id: 5,
+    icon: humanResources,
+    pageName: 'HR',
+  },
+  {
+    id: 6,
+    icon: technicalGuide,
+    pageName: 'Technical Guide',
+  },
+];
