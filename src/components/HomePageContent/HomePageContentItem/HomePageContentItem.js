25e0864cb8f2d08ca26ebc838fc5a07db67a36d2
Darya.Liashchynskaya <Dashalesh040600>
2022-04-15 16:40:05 +0300

    header logo

--- src/components/HomePageContent/HomePageContentItem/HomePageContentItem.js
+++ src/components/HomePageContent/HomePageContentItem/HomePageContentItem.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {COLORS, FONT_FAMILY, SIZES} from '../../../constants/style';
-
-const PAGE_ICON_SIZE = 45;
+import {CircleImage} from '../../CircleImage/CircleImage';
 
 export const HomePageContentItem = ({item, navigation}) => {
   return (
@@ -11,9 +10,7 @@ export const HomePageContentItem = ({item, navigation}) => {
       delayPressIn={0}
       onPress={() => navigation.navigate(item.routeName)}>
       <View style={styles.pagesListItem}>
-        <View style={styles.pageIconCircle}>
-          <Image source={item.icon} style={styles.pageIcon} />
-        </View>
+        <CircleImage image={item.icon} imageColor={COLORS.LIGHT_BLUE} />
         <Text style={styles.pageName}>{item.pageName}</Text>
       </View>
     </TouchableOpacity>
@@ -30,15 +27,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 10,
   },
-  pageIconCircle: {
-    width: PAGE_ICON_SIZE,
-    height: PAGE_ICON_SIZE,
-    borderRadius: PAGE_ICON_SIZE / 2,
-    backgroundColor: COLORS.LIGHT_BLUE,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  pageIcon: {width: 27, height: 27},
   pageName: {
     ...FONT_FAMILY,
     fontSize: SIZES.h4,
---
+++ src/constants/header_constants.js
@@ -0,0 +1,3 @@
+export const logo = require('../assets/header/logo.png');
+export const account = require('../assets/header/account.png');
+export const search = require('../assets/header/search.png');
