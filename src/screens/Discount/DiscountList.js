a740e62ab0a6bd839740d6e7249d8ded1556f3c6
Darya.Liashchynskaya <Dashalesh040600>
2022-04-19 10:46:12 +0300

    change naming for description cards

--- src/screens/Discount/DiscountList.js
+++ src/screens/Discount/DiscountList.js
@@ -1,34 +1,37 @@
-import {DISCOUNTS_DATA} from "../../constants/discount_constants";
-import {DiscountCard} from "./renderDiscountCard";
-import {FlatList, StyleSheet} from "react-native";
-import React from "react";
-import {SIZES} from "../../constants/style";
+import {DISCOUNTS_DATA} from '../../constants/discount_constants';
+import {DiscountCard} from './renderDiscountCard';
+import {FlatList, StyleSheet} from 'react-native';
+import React from 'react';
+import {SIZES} from '../../constants/style';
 
-
-export const DiscountList = ({ navigation }) => {
-    const numColumns = DISCOUNTS_DATA.length === 2 ? 1 : 2;
-    const renderDiscountCard = ({item}) => {
-        return (
-            <DiscountCard
-                {...item}
-                onPress={() => navigation.navigate('DiscountDesc', {
-                    itemId: item.id,
-                })}
-            />)
-    }
-    return <FlatList
-        data={DISCOUNTS_DATA}
-        keyExtractor={item => `${item.id}`}
-        renderItem={renderDiscountCard}
-        numColumns={numColumns}
-        contentContainerStyle={styles.container}
+export const DiscountList = ({navigation}) => {
+  const numColumns = DISCOUNTS_DATA.length === 2 ? 1 : 2;
+  const renderDiscountCard = ({item}) => {
+    return (
+      <DiscountCard
+        {...item}
+        onPress={() =>
+          navigation.navigate('DiscountDesc', {
+            itemRouteName: item.routeName,
+          })
+        }
+      />
+    );
+  };
+  return (
+    <FlatList
+      data={DISCOUNTS_DATA}
+      keyExtractor={item => `${item.id}`}
+      renderItem={renderDiscountCard}
+      numColumns={numColumns}
+      contentContainerStyle={styles.container}
     />
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 40,
-        paddingBottom: SIZES.HEIGHT * 0.3,
-    }
-})
-
+  container: {
+    marginTop: 40,
+    paddingBottom: SIZES.HEIGHT * 0.3,
+  },
+});
--- src/screens/DiscountDescPage/DiscountDescPage.js
+++ src/screens/DiscountDescPage/DiscountDescPage.js
@@ -1,50 +1,46 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState} from 'react';
 
-import {StyleSheet, View, Image } from "react-native";
-import {DiscountHeader} from "../Discount/DiscountHeader";
+import {StyleSheet, View, Image} from 'react-native';
+import {DiscountHeader} from '../Discount/DiscountHeader';
 
-import {COLORS, SIZES} from "../../constants/style";
-import {DISCOUNTS_DATA} from "../../constants/discount_constants";
-import {DiscountDescPageBody} from "./DiscountDescPageBody";
+import {COLORS, SIZES} from '../../constants/style';
+import {DISCOUNTS_DATA} from '../../constants/discount_constants';
+import {DiscountDescPageBody} from './DiscountDescPageBody';
 
 export const DiscountDescPage = ({route, navigation}) => {
-    const { itemId } = route.params;
-    const [discountData, setDiscountData] = useState(null);
+  const {itemRouteName} = route.params;
+  const [discountData, setDiscountData] = useState(null);
 
+  useEffect(() => {
+    //API call here
+    setDiscountData(
+      DISCOUNTS_DATA.filter(item => item.routeName === itemRouteName)[0],
+    );
+  }, []);
 
-    useEffect(() => {
-        //API call here
-        setDiscountData(DISCOUNTS_DATA.filter(item => item.id === itemId)[0])
-    }, [])
-
-    return (
-        <View
-            style={styles.wrapper}>
-            <DiscountHeader  navigation={navigation}/>
-            <Image
-                style={styles.logo}
-                source={discountData?.img}
-            />
-            <DiscountDescPageBody discountData={discountData}/>
-        </View>
-    )
+  return (
+    <View style={styles.wrapper}>
+      <DiscountHeader navigation={navigation} />
+      <Image style={styles.logo} source={discountData?.img} />
+      <DiscountDescPageBody discountData={discountData} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    wrapper: {
-        backgroundColor: 'white',
-    },
-    logo: {
-        position: 'absolute',
-        marginTop: 80,
-        width: SIZES.WIDTH / 2,
-        height: SIZES.WIDTH / 2,
-        borderRadius: 40,
-        borderWidth: 2,
-        borderStyle: 'solid',
-        borderColor: COLORS.DARK_GREY,
-        alignSelf: 'center',
-        zIndex: 1
-    },
-})
-
+  wrapper: {
+    backgroundColor: 'white',
+  },
+  logo: {
+    position: 'absolute',
+    marginTop: 80,
+    width: SIZES.WIDTH / 2,
+    height: SIZES.WIDTH / 2,
+    borderRadius: 40,
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: COLORS.DARK_GREY,
+    alignSelf: 'center',
+    zIndex: 1,
+  },
+});
